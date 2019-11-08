d8fb74e183d9509d7d008a674006586c0ad958d5
wiseman-why-not <marcus.e.wiseman@gmail.com>
2019-11-08 16:43:11 -0700

    added TODO's

--- src/RemoteControlApp.java
+++ src/RemoteControlApp.java
@@ -3,7 +3,8 @@ public class RemoteControlApp {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
 	}
-
+	// TODO: Add pressButton() method
+	// TODO: Add turnON() method
 }
